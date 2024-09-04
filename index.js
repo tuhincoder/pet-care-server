646fa3c563c44936d7736c0b25f59aba1a9496fc
ehsanhasan <mahmudulhasanehsan@gmail.com>
2024-09-04 16:45:55 +0600

    deployment setup

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const petCategoryCollection = client.db('petCare').collection('category')
@@ -151,14 +151,15 @@ async function run() {
         // all pets related api (total pets 16 )
         app.get('/api/v1/allPets-read', async (req, res) => {
             const filter = req.query;
-            console.log(filter);
-            const query = {
-                name: { $regex: filter.search, $options: 'i' }
-            }
+            console.log('throw error', filter);
+            // const query = {
+            //     name: { $regex: String(filter.search), $options: 'i' }
+            // }
+
             // if (filter) {
             //     query.filter = { $regex: filter.search, $options: 'i' }
             // }
-            const result = await allPetCollection.find(query).toArray();
+            const result = await allPetCollection.find().toArray();
             res.send(result)
         })
 
@@ -403,8 +404,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
